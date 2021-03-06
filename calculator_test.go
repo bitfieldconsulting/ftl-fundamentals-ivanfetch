9799fdc2be4e065c2e8807f4b51b4062664e4737
Ivan Fetch <ivan@ivanfetch.com>
2021-03-06 07:07:54 -0500

    Section 5: Add multiple test-cases and test descriptions
    Here is sample output from a failing test with its test description: `want 4.000000, got 5.000000, while testing two positive numbers which sum to a positive. The function call was: Add(3.000000, 2.000000)`

--- calculator_test.go
+++ calculator_test.go
@@ -7,29 +7,114 @@ import (
 
 // Note Fatal|FatalF are also useful methods,
 // to stop further execution within the same test.
+
+// Express test-case inputs and desired outputs
+type testCase struct {
+	a           float64
+	b           float64
+	want        float64
+	description string
+}
+
 func TestAdd(t *testing.T) {
+	// Define test cases
+	//
+	// Reference RE: `cases := []testCases` vs. `var cases ...`
+	// ways to define this variable: https://blog.golang.org/slices-intro
+	testCases := []testCase{
+		{
+			description: "two positive numbers which sum to a positive",
+			a:           2,
+			b:           2,
+			want:        4,
+		},
+		{
+			description: "a positive and negative number which sum to a positive",
+			a:           7,
+			b:           -2,
+			want:        5,
+		},
+		{
+			description: "a positive and negative number which sum to a negative",
+			a:           3,
+			b:           -5,
+			want:        -2,
+		},
+	}
+
 	t.Parallel()
-	var want float64 = 4
-	got := calculator.Add(2, 2)
-	if want != got {
-		t.Errorf("want %f, got %f", want, got)
+
+	// Fun fact: I initially called this variable `case` which is a keyword.
+	// The error did not make that obvious. Fun rabbit-hole.
+	for _, c := range testCases {
+		got := calculator.Add(c.a, c.b)
+		if got != c.want {
+			t.Errorf("want %f, got %f, while testing %s. The function call was: Add(%f, %f)", c.want, got, c.description, c.a, c.b)
+		}
 	}
 }
 
 func TestSubtract(t *testing.T) {
+	// Define test cases
+	testCases := []testCase{
+		{
+			description: "two positive numbers whos difference is negative",
+			a:           2,
+			b:           9,
+			want:        -7,
+		},
+		{
+			description: "two positive numbers whos difference is positive",
+			a:           7,
+			b:           2,
+			want:        5,
+		},
+		{
+			description: "one positive and one negative decimal number whos difference is a positive decimal",
+			a:           3,
+			b:           -2.5,
+			want:        5.5,
+		},
+	}
+
 	t.Parallel()
-	var want float64 = 2
-	got := calculator.Subtract(4, 2)
-	if want != got {
-		t.Errorf("want %f, got %f", want, got)
+
+	for _, c := range testCases {
+		got := calculator.Subtract(c.a, c.b)
+		if got != c.want {
+			t.Errorf("want %f, got %f, while testing %s. The function call was: Subtract(%f, %f)", c.want, got, c.description, c.a, c.b)
+		}
 	}
 }
 
 func TestMultiply(t *testing.T) {
+	// Define test cases
+	testCases := []testCase{
+		{
+			description: "two positive numbers whos product is positive",
+			a:           2,
+			b:           20,
+			want:        40,
+		},
+		{
+			description: " a positive and negative number whos product is negative",
+			a:           7,
+			b:           -2,
+			want:        -14,
+		},
+		{
+			description: "a positive decimal and negative decimal whos product is a negative decimal",
+			a:           8.4,
+			b:           -2.5,
+			want:        -21,
+		},
+	}
+
 	t.Parallel()
-	var want float64 = 6
-	got := calculator.Multiply(3, 2)
-	if want != got {
-		t.Errorf("want %f, got %f", want, got)
+	for _, c := range testCases {
+		got := calculator.Multiply(c.a, c.b)
+		if got != c.want {
+			t.Errorf("want %f, got %f, while testing %s. The function call was: Multiply(%f, %f)", c.want, got, c.description, c.a, c.b)
+		}
 	}
 }
