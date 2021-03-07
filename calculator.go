e07a035bf14e27622261d20df5d598bb6aac6ba8
Ivan Fetch <ivan@ivanfetch.com>
2021-03-07 06:19:23 -0500

    Section 7: Add(), Subtract(), Multiply(), and Divide() accept 2+ arguments (variadic)
    I updated the functions first to verify tests still pass.

--- calculator.go
+++ calculator.go
@@ -8,29 +8,62 @@ import (
 )
 
 // Add takes two numbers and returns the result of adding them together.
-func Add(a, b float64) float64 {
-	return a + b
+func Add(a float64, b ...float64) float64 {
+	r := a + b[0]
+
+	// If b contains more than 1 element,
+	// call ourself with the current result and those remaining elements.
+	if len(b[1:]) > 0 {
+		r = Add(r, b[1:]...)
+	}
+	return r
 }
 
 // Subtract takes two numbers and returns the result of subtracting the second
 // from the first.
-func Subtract(a, b float64) float64 {
-	return a - b
+func Subtract(a float64, b ...float64) float64 {
+	r := a - b[0]
+
+	// If b contains more than 1 element,
+	// call ourself with the current result and those remaining elements.
+	if len(b[1:]) > 0 {
+		r = Subtract(r, b[1:]...)
+	}
+	return r
 }
 
 // Multiply takes two numbers and returns the result of multiplying them together.
-func Multiply(a, b float64) float64 {
-	return a * b
+func Multiply(a float64, b ...float64) float64 {
+	r := a * b[0]
+
+	// If b contains more than 1 element,
+	// call ourself with the current result and those remaining elements.
+	if len(b[1:]) > 0 {
+		r = Multiply(r, b[1:]...)
+	}
+	return r
 }
 
 // Divide takes two numbers and returns the result of dividing the second
 // from the first.
-func Divide(a, b float64) (error, float64) {
-	if b == 0 {
-		return errors.New(fmt.Sprintf("divide-by-zero for Divide(%f, %f)", a, b)), 0.0
+func Divide(a float64, b ...float64) (error, float64) {
+	var err error // potential error from recursive call to Divide()
+
+	if b[0] == 0 {
+		return errors.New(fmt.Sprintf("divide-by-zero for Divide(%f, %f)", a, b[0])), 0.0
 	}
 
-	return nil, a / b
+	r := a / b[0]
+
+	// If b contains more than 1 element,
+	// call ourself with the current result and those remaining elements.
+	if len(b[1:]) > 0 {
+		err, r = Divide(r, b[1:]...)
+		if err != nil {
+			return err, r
+		}
+	}
+	return nil, r
 }
 
 // Sqrt returns the square root of a number
